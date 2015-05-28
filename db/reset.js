22e7a071ca4e64d2c3cb61fa6f4c35632fb0d66c
rushindrasinha <gaape@live.com>
2015-05-27 18:05:44 -0700

    Local User Authentication & Multiple Node Packages

---
+++ db/reset.js
@@ -0,0 +1,22 @@
+// Connect to mongo
+mongoose = new Mongo("localhost");
+
+// Destroy database
+project = mongoose.getDB("Project04");
+project.dropDatabase();
+
+// Create the database
+project = mongoose.getDB("Project04");
+project.createCollection("posts");
+
+// Create seeds array
+var posts = [
+  {title: "My First", body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", comments: []},
+  {title: "My Second", body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", comments: []},
+  {title: "My Third", body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", comments: []}
+];
+
+// Seed posts collection
+posts.forEach(function(post){
+  project.posts.insert(post);
+});
