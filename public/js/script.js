5e1d330a218334391b9a0da3f19257d7599b1514
kyleconkright <kyleconkright@gmail.com>
2015-05-29 11:44:34 -0700

    semantic login and signup

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,18 @@
-// $.ajax({
-//   type: GET,
-//   dataType: "json",
-//   url: "https://api.locu.com/v1_0/venue/6e55d8e2c73ef1966b05/?api_key=e24ffeac95907824f1503d2a97cb39d5c9e28e38",
-//   success: function(result){
-//     console.log(result);
-//   }
-// });
-console.log("Works");
+$(document).ready(function(){
+
+	$('#login-button').on('click', function(e){
+		e.preventDefault();
+		$('#login-form')
+			.modal('setting', 'transition', 'horizontal flip')
+			.modal('show');
+	});
+
+	$('#signup-button').on('click', function(e){
+		e.preventDefault();
+		$('#signup-form')
+			.modal('setting', 'transition', 'horizontal flip')
+			.modal('show');
+	});
+
+});
+
