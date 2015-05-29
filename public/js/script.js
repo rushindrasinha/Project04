be10c530e3d94bdb9829a7d961d0c89aaf0ed469
rushindrasinha <gaape@live.com>
2015-05-28 17:35:20 -0700

    Added Script tags for JQ and created public folder

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+// $.ajax({
+//   type: GET,
+//   dataType: "json",
+//   url: "https://api.locu.com/v1_0/venue/6e55d8e2c73ef1966b05/?api_key=e24ffeac95907824f1503d2a97cb39d5c9e28e38",
+//   success: function(result){
+//     console.log(result);
+//   }
+// });
+console.log("Works");
