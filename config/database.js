821972cca6a3e79a0efd5cc3270197624cb00c99
rushindrasinha <gaape@live.com>
2015-06-04 19:51:23 -0700

    Changed database to production

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-  'url' : 'mongodb://localhost:27017/Project04'
+  'url' : 'mongodb://heroku_app37528691:7iqgrdd58srbiflcftgc3l6vql@ds043012.mongolab.com:43012/heroku_app37528691'
+  // 'url' : 'mongodb://localhost:27017/Project04'
 }
